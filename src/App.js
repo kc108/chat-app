97385a3774a91c97ef13f24b3a5305b7229dffaf
Kim Carpico <kcarpico@gmail.com>
2021-09-24 16:08:35 -0700

    created public and private routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+// import { BrowserRouter } from 'react-router-dom';
 import 'rsuite/dist/rsuite.min.css';
 import './styles/main.scss';
+import { Switch } from 'react-router';
+import Signin from './pages/Signin';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Switch>
+      <PublicRoute path="/signin">
+        <Signin />
+      </PublicRoute>
+
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
---
+++ src/pages/Signin.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Signin = () => {
+  <div>Sign in</div>;
+};
+
+export default Signin;
