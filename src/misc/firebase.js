59094d7111cde1739396d005832758353b1c866a
Kim Carpico <kcarpico@gmail.com>
2021-09-24 13:32:05 -0700

    initialized firebase setup and added CDN to config files

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,14 +1,12 @@
-import firebase from "firebase/app"
+import firebase from 'firebase/app';
 
 const config = {
-  apiKey: 'AIzaSyAKGRssxY40W9um6R1SbbQ1Fh_1gdWfoPs',
-  authDomain: ''chat-web-app-4ee4c.firebaseapp.com,
-  databaseURL: 'https://chat-web-app-4ee4c.appspot.com',
-  projectID: 'chat-web-app-4ee4c',
-  storageBucket: 'chat-web-app-4ee4c.appspot.com',
-  messagingSenderId: '167319830934',
-  appId: '1:167319830934:web:419e220f9fdcd15ea25db7',
+  apiKey: 'AIzaSyBrMn_rKTvYre8yZYC7hAGR1zIFo-_n7Ik',
+  authDomain: 'chat-web-app-89bb4.firebaseapp.com',
+  projectId: 'chat-web-app-89bb4',
+  storageBucket: 'chat-web-app-89bb4.appspot.com',
+  messagingSenderId: '931156510021',
+  appId: '1:931156510021:web:579b783679d3bf0bf42a2c',
 };
-
 
 const app = firebase.initializeApp(config);
